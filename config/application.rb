565e6e029b21eee96c091e72da0d839895c6cc33
Kota Kamiyama <k.kamikami1211@gmail.com>
2021-02-11 18:21:56 +0900

    application.rbを修正

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,19 @@ module Tudukeyo
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+    config.generators do |g|
+      g.template_engine false
+      g.javascripts false
+      g.stylesheets false
+      g.helper false
+      g.test_framework :rspec,
+                       view_specs: false,
+                       routing_specs: false,
+                       helper_specs: false,
+                       controller_specs: false,
+                       request_specs: true
+    end
+
     config.api_only = true
   end
 end
