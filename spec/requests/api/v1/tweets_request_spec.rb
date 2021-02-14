d6b30dd2b31ec9f399c98bd8036917ceb00bcf9d
Kota Kamiyama <k.kamikami1211@gmail.com>
2021-02-14 23:49:06 +0900

    一覧取得のテストを修正

--- spec/requests/api/v1/tweets_request_spec.rb
+++ spec/requests/api/v1/tweets_request_spec.rb
@@ -4,20 +4,19 @@ RSpec.describe "Api::V1::Tweets", type: :request do
   describe "GET api/v1/tweets" do
     subject { get(api_v1_tweets_path) }
 
-    let(:tweet1) { create(:tweet, created_at: 2.days.ago) }
-    let(:tweet2) { create(:tweet, created_at: 1.days.ago) }
-    let(:tweet3) { create(:tweet) }
+    let!(:tweet1) { create(:tweet, created_at: 2.days.ago) }
+    let!(:tweet2) { create(:tweet, created_at: 1.days.ago) }
+    let!(:tweet3) { create(:tweet) }
     it "ツイートの一覧を取得できる" do
       subject
 
       res = JSON.parse(response.body)
       # idだけ抜き出した配列
       res_id_array = res.map {|h| h["id"] }
-
       expect(res.length).to eq 3
       expect(res[0].keys).to eq ["id", "content", "created_at", "user"]
       expect(res[0]["user"].keys).to eq ["id", "name", "profile", "avatar"]
-      expect(res_id_array).to eq [tweet_3.id, tweet_2.id, tweet_1.id]
+      expect(res_id_array).to eq [tweet3.id, tweet2.id, tweet1.id]
       expect(response).to have_http_status :ok
     end
   end
